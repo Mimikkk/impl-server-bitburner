a821b1eb6e682390d5f64e4281a25c4367d54489
Daniel Zdancewicz <inwazzin@gmail.com>
2025-03-09 21:09:18 +0100

    foo

--- src/server/routing/routes/http.routes.ts
+++ src/server/routing/routes/http.routes.ts
@@ -1,11 +1,10 @@
 import { InstructionController } from "@server/controllers/http/InstructionController.ts";
 import { HttpRouterBuilder } from "@server/routing/impl/routers/http/HttpRouter.builder.ts";
-import { HttpMethod } from "@shared/enums/HttpMethod.enum.ts";
 import { SocketConnectionController } from "@server/controllers/http/SocketConnectionController.ts";
 
 export const http = HttpRouterBuilder.create()
-  .add(HttpMethod.Get, "/", InstructionController, "index")
-  .add(HttpMethod.Get, "/api/connections", SocketConnectionController, "index")
-  .add(HttpMethod.Get, "/api/connections/{id}", SocketConnectionController, "show")
-  .add(HttpMethod.Post, "/api/connections/{id}/commands/{command}", SocketConnectionController, "commands")
+  .get("/", InstructionController, "index")
+  .get("/api/connections", SocketConnectionController, "index")
+  .get("/api/connections/{id}", SocketConnectionController, "show")
+  .post("/api/connections/{id}/commands/{command}", SocketConnectionController, "commands")
   .build();
