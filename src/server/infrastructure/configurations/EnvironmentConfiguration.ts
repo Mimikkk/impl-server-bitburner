59cb42159a0d27be8eeea881348975fe5cf2c089
Mimikkk <zdancewicz.daniel@gmail.com>
2025-04-24 21:57:33 +0200

    make sample env

--- src/server/infrastructure/configurations/EnvironmentConfiguration.ts
+++ src/server/infrastructure/configurations/EnvironmentConfiguration.ts
@@ -1,4 +1,4 @@
 export namespace EnvironmentConfiguration {
-  export const port = parseInt(Deno.env.get("SERVER_PORT") ?? "8080");
-  export const hostname = Deno.env.get("SERVER_HOST") ?? "127.0.0.1";
+  export const port = +(Deno.env.get("SERVER_PORT") || "8080");
+  export const hostname = Deno.env.get("SERVER_HOST") || "127.0.0.1";
 }
